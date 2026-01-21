c0b48672ece9d7385af638b0871f8737d15b704a
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-01-21 15:47:31 -0300

    feat(flake): add flake and devshell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShellNoCC {
+          name = "dev";
+          packages = with pkgs; [
+            dotnetCorePackages.sdk_10_0
+          ];
+        };
+      };
+    };
+}
