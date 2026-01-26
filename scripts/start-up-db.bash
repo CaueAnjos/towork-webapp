6ccb44f0aa3b6913b12ee1f7d2c1fb3eba257b17
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-01-26 12:30:45 -0300

    feat(model): configure postgresql deb db

--- scripts/start-up-db.bash
+++ scripts/start-up-db.bash
@@ -2,5 +2,8 @@
 
 podman run \
     --name towork-webapp-db \
-    -e POSTGRES_PASSWORD="caue1951[]" \
+    -e POSTGRES_PASSWORD="caue1951" \
+    -e POSTGRES_USER="podman" \
+    -e POSTGRES_DB="towork" \
+    -p 5432:5432 \
     -d --rm postgres
