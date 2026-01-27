80c9f98e1de11db782f6e4af62508acbbfef6d0c
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-01-27 14:27:42 -0300

    fix(packages): fix mvc package

--- packages/mvc.nix
+++ packages/mvc.nix
@@ -1,16 +1,36 @@
 {
+  lib,
   buildDotnetModule,
   dotnetCorePackages,
-}:
-buildDotnetModule rec {
-  pname = "ToworkMVC";
-  version = "0.1.0";
-  src = ../src/ToworkMVC;
-  dotnet-sdk = dotnetCorePackages.sdk_10_0;
-  nugetDeps = ./deps.json;
-  makeWrapperArgs = [
-    "--set"
-    "DOTNET_CONTENTROOT"
-    "${placeholder "out"}/lib/${pname}"
+}: let
+  inherit (lib.fileset) toSource unions fileFilter;
+  inherit (lib.fileset) trace;
+
+  # NOTE: the root dir of the project!
+  root = ../src/ToworkMVC;
+
+  src = builtins.path {
+    path = toSource {inherit root fileset;};
+    name = "mvc-root";
+  };
+
+  fileset = unions [
+    (fileFilter (file: file.hasExt "cs" || file.hasExt "csproj") root)
+    (root + "/Views")
+    (root + "/wwwroot")
   ];
-}
+in
+  trace fileset
+  buildDotnetModule rec {
+    pname = "ToworkMVC";
+    version = "0.1.0";
+    inherit src;
+    dotnet-sdk = dotnetCorePackages.sdk_10_0;
+    dotnet-runtime = dotnetCorePackages.aspnetcore_10_0-bin;
+    nugetDeps = ./deps.json;
+    makeWrapperArgs = [
+      "--set"
+      "DOTNET_CONTENTROOT"
+      "${placeholder "out"}/lib/${pname}"
+    ];
+  }
